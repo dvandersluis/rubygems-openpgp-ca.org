342d9b1143b329bd015f64c783aafefc52fe1fd5
Grant Olson <kgo@grant-olson.net>
2013-02-10 08:34:49 -0500

    Initial checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OpenpgpCa::Application.config.secret_token = 'acc05be0e669894c7c64ea09650537e61897d1a9ad3e2d9d1a7cb773cf62ad79d2056df30d2c0b12b189a6863fcf50cacf752efdf257a21135a0f58d39fc3e6a'
