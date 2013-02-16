31c22b6e20fb008c8da76a25d900c16dc3dd46dc
Grant Olson <kgo@grant-olson.net>
2013-02-16 10:51:40 -0500

    Get email working

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ OpenpgpCa::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
@@ -36,5 +36,7 @@ OpenpgpCa::Application.configure do
   config.assets.debug = true
 
   # Mailer url for devise
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :sendmail
 end
