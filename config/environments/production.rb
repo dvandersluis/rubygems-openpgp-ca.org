84ae7a2ccb177b2a40a3001440aeac72d95a48d5
Grant Olson <kgo@grant-olson.net>
2013-02-17 15:02:12 -0500

    Email fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,8 @@ OpenpgpCa::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { :host => 'rubygems-openpgp-ca.org' }
 
   ActionMailer::Base.smtp_settings = {
     :address => ENV['MAIL_SERVER'],
