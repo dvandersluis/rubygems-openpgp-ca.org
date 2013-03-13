4dcd79ab0dfe3b3959668d0a656416893f53a50f
Grant Olson <grant@pikimal.com>
2013-03-13 10:55:46 -0400

    Right links in mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ OpenpgpCa::Application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { :host => 'rubygems-openpgp-ca.org' }
+  config.action_mailer.default_url_options = { :host => 'www.rubygems-openpgp-ca.org', :protocol => 'https' }
 
   ActionMailer::Base.smtp_settings = {
     :address => ENV['MAIL_SERVER'],
